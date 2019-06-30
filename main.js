de53f1c864fe3b074c285a253b4d94e7a89d80e1
Radosław Kierznowski <radek.kierznowski@outlook.com>
2019-06-30 21:59:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('siema');
+const name='Radosław Kierznowski';
+const age=25;
+
+console.log(`Nazywam się ${name} i mam ${age} lat. Widzę, że jesteś poszukiwaczem Easter Eggów. Bardzo miło mi Cię tu widzieć.`);
