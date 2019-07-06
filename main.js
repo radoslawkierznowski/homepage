5b57b29630b730eb0f846410e3b24b9282ad8493
Radosław Kierznowski <radek.kierznowski@outlook.com>
2019-07-06 20:46:38 +0200

    Podstawy logiki w JS

--- main.js
+++ main.js
@@ -6,9 +6,29 @@ console.log(`Nazywam się ${name} i mam ${age} lat. Widzę, że jesteś poszukiw
 
 const main__div = document.querySelector('.main__div--js');
 
-main__div.innerHTML='To zdanie zostało napisane przy użyciu JS 😄';
+main__div.innerHTML = 'To zdanie zostało napisane przy użyciu JS 😄';
 
 const paragraphs = document.querySelectorAll('p');
 
 console.log(paragraphs);
 console.log(paragraphs[3]);
+
+if ('javascript' != 'java') { console.log('to prawda') }
+
+switch (age) {
+    case 20:
+        console.console.log('Masz 20 lat.');
+        break;
+
+    case 30:
+        console.log('Masz 30 lat.');
+        break;
+
+    default:
+        console.log(`Masz ${age} lat.`);
+
+        break;
+}
+
+const amIOld = age > 70 ? 'yes' : 'no';
+console.log(amIOld);
