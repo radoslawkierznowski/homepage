374ca3a9ddea78a8bedf763a2a8f9d05a76a7606
Radosław Kierznowski <radek.kierznowski@outlook.com>
2019-07-06 18:08:12 +0200

    DOM

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 console.log('siema');
-const name='Radosław Kierznowski';
-const age=25;
+const name = 'Radosław Kierznowski';
+const age = 25;
 
 console.log(`Nazywam się ${name} i mam ${age} lat. Widzę, że jesteś poszukiwaczem Easter Eggów. Bardzo miło mi Cię tu widzieć.`);
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = 'Nazywam się <strong>${name}</strong> i mam ${age} lat.';
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[3]);
